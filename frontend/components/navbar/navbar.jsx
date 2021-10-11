6eb8108be588325c812095677b242bb8d5e85751
elsicab <ecaba006@gmail.com>
2021-10-11 14:36:34 -0400

    My Network is fully functional

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -1,18 +1,8 @@
 import React from 'react';
 import { FaSearch } from 'react-icons/fa';
 import { ImHome3 } from 'react-icons/im';
-import { BsBriefcaseFill } from 'react-icons/bs';
-import { AiFillMessage } from 'react-icons/ai';
-import { FaBell } from 'react-icons/fa';
-import { withRouter } from 'react-router';
 import { BsPeopleFill } from 'react-icons/bs';
-import { logout } from '../../actions/session_actions';
 import { Link } from 'react-router-dom';
-import { BsPersonBoundingBox } from 'react-icons/bs'
-
-
-
-
 
 class Navbar extends React.Component {
     constructor(props){
@@ -27,7 +17,6 @@ class Navbar extends React.Component {
     handleSubmit(e){
         e.preventDefault();
         this.props.logout()
-            // .then(() => this.props.history.push('/'));
     };
 
     handleDropdown(e) {
@@ -52,28 +41,17 @@ class Navbar extends React.Component {
                     <div className="right_nav">
                         <ul className="navbar_icons">
                             <li className="single_icon">
-                                <Link id="single_icon"to='/feed'>
+                                <Link id="single_icon" to='/feed'>
                                     <p className="navbar_icon"><ImHome3 /></p>
                                     <p className="title">Home</p>
                                 </Link>
                             </li>
                             <li className="single_icon">
-                                <p className="navbar_icon"><BsPeopleFill /></p>
-                                <p className="title">My Network</p>
+                                <Link id="network" to='/myNetwork'>
+                                    <p className="navbar_icon"><BsPeopleFill /></p>
+                                    <p className="title">My Network</p>
+                                </Link>
                             </li>
-                            {/* <li className="single_icon">
-                                <p className="navbar_icon"><BsBriefcaseFill /></p>
-                                <p className="title">Jobs</p>
-                            </li>
-                            <li className="single_icon">
-                                <p className="navbar_icon"><AiFillMessage /></p>
-                                <p className="title">Messaging</p>
-                            </li>
-                            <li className="single_icon">
-                                <p className="navbar_icon"><FaBell /></p>
-                                <p className="title">Notifications</p>
-                            </li> */}
-                            {/* <li className="single_icon" id="logout_button" onClick={this.handleSubmit}><Link to='/'>Logout!</Link></li> */}
                             <li className="dropdown_menu">
                                 <div className="avatar_dropdown" onClick={this.handleDropdown} >
                                     <div>{menuAvatar}</div>
