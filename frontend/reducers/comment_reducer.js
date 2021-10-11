94d2a6eb24275654afa0568276d2778ecb48c720
elsicab <ecaba006@gmail.com>
2021-10-10 23:03:47 -0400

    Comments index rerender properly

--- frontend/reducers/comment_reducer.js
+++ frontend/reducers/comment_reducer.js
@@ -2,14 +2,19 @@ import {RECEIVE_COMMENT, RECEIVE_COMMENTS, DELETE_COMMENT} from '../actions/comm
 
 const commentReducer = (state = {}, action) => {
     Object.freeze(state)
+    let newState = Object.assign({}, state)
     switch(action.type){
         case RECEIVE_COMMENTS:
             return action.comments
         case RECEIVE_COMMENT:
-            return Object.assign({}, state, action.comment)
+            // return Object.assign({}, state, action.comment)
+            newState[action.comment.id] = action.comment
+            return newState
         case DELETE_COMMENT:
-            let newState = []
-            return newState.filter(element => element !== action.commentId);
+            // let newState = []
+            // return newState.filter(element => element !== action.commentId);
+            delete newState[action.commentId]
+            return newState
         default:
             return state;
     }
